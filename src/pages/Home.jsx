91e928554f3801f3a88611766d78edec42638243
waltershaquil <81990995+waltershaquil@users.noreply.github.com>
2025-04-23 21:01:27 +0200

    background changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import HeaderTop from "../components/HeaderTop";
 function Home() {
   return (
     <div className="relative min-h-screen">
-      <div className="fixed inset-0 -z-10 w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <div className="fixed min-h-screen inset-0 -z-10 w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
       <HeaderTop />
       <DescriptioAndPhoto />
       <FooterBottom />
