9860c9d6074590178e49ff53d128bbb14bdf9b20
waltershaquil <81990995+waltershaquil@users.noreply.github.com>
2025-04-23 21:06:01 +0200

    lilte

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
+    <div >
+      <div className="fixed min-h-screen inset-0 -z-10 w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
       <Home />
     </div>
   );
